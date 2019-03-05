1fc1288deac940f21135748dca70e6c41c85b10f
Kabir <kabir@kabirkuriyan.com>
2019-03-04 19:40:32 -0500

    Adding invalidation to deploy script

--- frontend/sponsorship-deploy.sh
+++ frontend/sponsorship-deploy.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 # Sponsorship deploy script
-# It is expected that KEY and SECRET will be set in the environment
 
 npm run-script build
-AWS_ACCESS_KEY_ID="$KEY" AWS_SECRET_ACCESS_KEY="$SECRET"
-#empty bucket, then upload
-echo $KEY
-echo $SECRET
 
+KEY="AWS_ACCESS_KEY_ID"
+SECRET="AWS_ACCESS_KEY_ID"
+
+#empty bucket, then upload
 AWS_ACCESS_KEY_ID="$KEY" AWS_SECRET_ACCESS_KEY="$SECRET" aws s3 rm s3://hackru-sponsorship --recursive
 AWS_ACCESS_KEY_ID="$KEY" AWS_SECRET_ACCESS_KEY="$SECRET" aws s3 cp --recursive build s3://hackru-sponsorship --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-# aws cloudfront create-invalidation --distribution-id "E1YIAV5VULIMY7"  --paths "/*"
+
+# cloudfront invalidation
+AWS_ACCESS_KEY_ID="$KEY" AWS_SECRET_ACCESS_KEY="$SECRET" aws cloudfront create-invalidation --distribution-id "E1KP0GPI31U70U"  --paths "/*"
